d38b6ba486e515be4bef7f56cbc2177b4ef1e8b6
javier vilchis <javivilchis@gmail.com>
2021-09-09 15:25:55 -0500

    adding server files

---
+++ routes/api.js
@@ -0,0 +1,90 @@
+const router = require("express").Router();
+const Transaction = require("../models/transaction.js");
+
+router.post("/api/createWorkout", ({ body }, res) => {
+     Transaction.create(body)
+          .then(dbTransaction => {
+               res.json(dbTransaction);
+          })
+          .catch(err => {
+               res.status(400).json(err);
+          });
+});
+router.put("/api/addExercise", ({ body }, res) => {
+
+});
+router.post("/api/transaction/bulk", ({ body }, res) => {
+     Transaction.insertMany(body)
+          .then(dbTransaction => {
+               res.json(dbTransaction);
+          })
+          .catch(err => {
+               res.status(400).json(err);
+          });
+});
+
+router.get("/api/getWorkoutsInRange", (req, res) => {
+     Transaction.find({})
+          .sort({ date: -1 })
+          .then(dbTransaction => {
+               res.json(dbTransaction);
+          })
+          .catch(err => {
+               res.status(400).json(err);
+          });
+});
+
+module.exports = router;
+
+/* for example */
+/*
+db.User.create({ name: "Ernest Hemingway" })
+          .then(dbUser => {
+               console.log(dbUser);
+          })
+          .catch(({ message }) => {
+               console.log(message);
+          });
+
+app.get("/notes", (req, res) => {
+     db.Note.find({})
+          .then(dbNote => {
+               res.json(dbNote);
+          })
+          .catch(err => {
+               res.json(err);
+          });
+});
+
+app.get("/user", (req, res) => {
+     db.User.find({})
+          .then(dbUser => {
+               res.json(dbUser);
+          })
+          .catch(err => {
+               res.json(err);
+          });
+});
+
+app.post("/submit", ({ body }, res) => {
+     db.Note.create(body)
+          .then(({ _id }) => db.User.findOneAndUpdate({}, { $push: { notes: _id } }, { new: true }))
+          .then(dbUser => {
+               res.json(dbUser);
+          })
+          .catch(err => {
+               res.json(err);
+          });
+});
+
+app.get("/populateduser", (req, res) => {
+     db.User.find({})
+          .populate("notes")
+          .then(dbUser => {
+               res.json(dbUser);
+          })
+          .catch(err => {
+               res.json(err);
+          });
+});
+*/
