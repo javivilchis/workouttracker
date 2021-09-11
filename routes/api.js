5d88c32b1215822d3eeb5178543a17b3d1717027
javier vilchis <javivilchis@gmail.com>
2021-09-11 15:40:47 -0500

    removing the comments

--- routes/api.js
+++ routes/api.js
@@ -1,24 +1,17 @@
 const router = require("express").Router();
-//const { Workout } = require("../models/index.js");
 const db = require("../models/index.js");
 
 router.get("/api/workouts/range", async (req, res) => {
      const result = await db.Workout.find({}).sort({ day: -1 }).limit(10);
-     res.send(result);
+     res.send(result.reverse());
 });
 
 router.get("/api/workouts", async (req, res) => {
-     // let remove = await db.Workout.remove({ _id: "613cde0919325f3c20c4f710" });
-     // res.send(remove);
-
      let response = await db.Workout.find({});
      res.send(response);
-
 });
 
 router.post("/api/workouts/", (req, res) => {
-     console.log("add workout");
-     console.log(req.body);
      db.Workout.create(req.body)
           .then(dbWorkout => {
                res.json(dbWorkout);
@@ -28,28 +21,13 @@ router.post("/api/workouts/", (req, res) => {
           });
 });
 router.put("/api/workouts/:id", async (req, res) => {
-     console.log("PUT api");
 
      let workout = await db.Workout.findOne({
           _id: req.params.id,
      });
-
-     console.log("Workout ID: ", workout);
-     console.log("Workout Exercise: ", req.body);
-     console.log("*****************************");
-
-     // if (workout.exercises) {
      workout.exercises.push(req.body);
-     // } else {
-     //      workout.exercises = [];
-     //      console.log(workout.exercises)
-     //      workout.exercises.push(req.body);
-     // }
-
-     console.log(workout);
 
      let result = await workout.save();
-     console.log(result);
      res.send(result);
 
 
@@ -57,7 +35,6 @@ router.put("/api/workouts/:id", async (req, res) => {
 router.delete("/api/workouts/delete/:id", async (req, res) => {
      // hardcode an ID or pass in an ID to remove
      let id = req.params.id;
-
      let remove = await db.Workout.deleteOne({ _id: id });
      res.send(remove);
 })
